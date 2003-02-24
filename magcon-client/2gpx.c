040de0deac070e2e3b28960aa2f834676ff8ecc3
niki <niki>
2003-02-24 18:12:11 +0000

    GPX and CSV output

--- magcon-client/2gpx.c
+++ magcon-client/2gpx.c
@@ -1,4 +1,4 @@
-/* $Id: 2gpx.c,v 1.2 2003/02/18 18:07:26 niki Exp $ */
+/* $Id: 2gpx.c,v 1.3 2003/02/24 18:12:11 niki Exp $ */
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <fcntl.h>
@@ -19,6 +19,8 @@
 #if !defined(O_BINARY)
 	#define O_BINARY 0
 #endif
+
+typedef void(*_parsemagconffile)(struct pdb *pdb, const int fd);
 
 static double mag2degrees(double mag_val) /*{{{1*/
 {
@@ -80,7 +82,58 @@ static int parsemagdatastring(const char *data, struct trkrec *trk){ /*{{{1*/
         return ret;
 }
 
-void parsemagconfile(struct pdb *pdb, const int fd){ /*{{{1*/
+void parsemagconfileimapsend(struct pdb *pdb, const int fd){ /*{{{1*/
+        struct trkrec trk;
+        struct maghead hdr = {13, "4D533336 MS", "36",2};
+	char *fname="magcon";
+        int j,reccount;
+        char buff[PUFFSIZE];
+        unsigned char len;
+        struct pdb_record *pdb_rec;
+
+        memset(buff,0,PUFFSIZE);
+        j=0;
+        reccount=0;
+
+        strcat(buff,fname);
+        len=strlen(buff);
+        write(fd,&hdr,sizeof(struct maghead));
+        write(fd,&len,sizeof(len));
+        write(fd,buff,len);
+        write(fd,&j,sizeof(int));
+
+        for(pdb_rec = pdb->rec_index.rec; pdb_rec; pdb_rec=pdb_rec->next) {
+
+                if(parsemagdatastring((char*)pdb_rec->data,&trk)){
+                        write(fd,&trk,sizeof(struct trkrec));
+                        reccount++;
+                }
+        }
+        lseek(fd,sizeof(struct maghead)+sizeof(len)+len,SEEK_SET);
+        write(fd,&reccount,sizeof(int));
+}
+
+void parsemagconfilecsv(struct pdb *pdb, const int fd){ /*{{{1*/
+	struct trkrec trk;
+	int reccount;
+        struct pdb_record *pdb_rec;
+	char buff[1024];
+	char buff2[1024];
+	char *trkpt="%f,%f,%f,%s.%dZ\n";
+	reccount=0;
+
+	for(pdb_rec = pdb->rec_index.rec; pdb_rec; pdb_rec=pdb_rec->next) {
+	
+		if(parsemagdatastring((char*)pdb_rec->data,&trk)){
+			memset(buff,0,1024);
+			strftime(buff2,1024,"%Y-%m-%dT%H:%M:%S",gmtime(&trk.time));
+			sprintf(buff,trkpt,-trk.y,trk.x,trk.altitude,buff2,trk.centisecs/3*5);
+			write(fd,buff,strlen(buff));
+			reccount++;
+		}
+	}
+}
+void parsemagconfilegpx(struct pdb *pdb, const int fd){ /*{{{1*/
 	struct trkrec trk;
 	int reccount;
         struct pdb_record *pdb_rec;
@@ -112,43 +165,50 @@ int main(int argcount,char* arg[]){ /*{{{1*/
 	struct stat sta;
 	int fd,fd2;
 	struct pdb *pdb;
+	_parsemagconffile parsemagconfile;
 
 	memset(&sta,0,sizeof(struct stat));
 
-	if (argcount>=2){
-		if(stat(arg[1],&sta)==0 && S_ISREG(sta.st_mode)){
-			fd=open(arg[1],O_RDONLY);
-			if(fd!=-1){
-				if (NULL == (pdb = pdb_Read(fd))) {
-					printf("Could not read pdbfile.\n");
-					close(fd);
-					return 1;
-				}
+	if (argcount<4){ 
+		printf("\nUsage: %s <track|csv|mapsend> <inputfile> <ouputfile>\n\n",arg[0]); 
+		return 1;
+	}
+	
+	if(strcmp("csv",arg[1])==0) parsemagconfile=parsemagconfilecsv;
+	else if(strcmp("track",arg[1])==0) parsemagconfile=parsemagconfilegpx;
+	else if(strcmp("mapsend",arg[1])==0) parsemagconfile=parsemagconfileimapsend;
+	else {printf("Unknown ouputformat \"%s\"\n",arg[1]); return 1;}
 
-				if ((pdb->creator != AppId) || (pdb->type != Data)) {
-					printf("Not a MagCon file.\n");
-					close(fd);
-					return 1;
-				}
-
-				fd2=argcount>=3?open(arg[2],O_WRONLY|O_CREAT|O_TRUNC|O_BINARY,S_IRUSR|S_IWUSR):STDIN_FILENO;
-				if(fd2!=-1){
-					parsemagconfile(pdb,fd2);
-					close(fd2);
-
-				} else {
-					perror(NULL);
-				}
+	if(stat(arg[2],&sta)==0 && S_ISREG(sta.st_mode)){
+		fd=open(arg[2],O_RDONLY|O_BINARY);
+		if(fd!=-1){
+			if (NULL == (pdb = pdb_Read(fd))) {
+				printf("Could not read pdbfile.\n");
+				close(fd);
+				return 1;
 			}
-			close(fd);
-		} else {
-			perror(NULL);
+
+			if ((pdb->creator != AppId) || (pdb->type != Data)) {
+				printf("Not a MagCon file.\n");
+				close(fd);
+				return 1;
+			}
+
+			fd2=argcount>=4?open(arg[3],O_WRONLY|O_CREAT|O_TRUNC|O_BINARY,S_IRUSR|S_IWUSR):STDIN_FILENO;
+			if(fd2!=-1){
+				parsemagconfile(pdb,fd2);
+				close(fd2);
+
+			} else {
+				perror(NULL);
+			}
 		}
+		close(fd);
 	} else {
-		printf("\nUsage: %s <inputfile> [<ouputfile>] \n\n",arg[0]);
+		perror(NULL);
 	}
 
-	return 0;
+return 0;
 }
 
 /* vim: set fdm=indent:*/
